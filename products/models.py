25a577621267f4992dab75d055f3d08ef662bdb8
Sri Ranga <sriranga451@gmail.com>
2024-07-30 19:15:52 +0530

    product models created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.conf import settings
 
-# Create your models here.
+
+
+
+class BaseTimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class BaseUserTrackedModel(models.Model):
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, default=None, on_delete=models.SET_NULL, related_name="created_%(class)s")
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, default=None, on_delete=models.SET_NULL, related_name="updated_%(class)s")
+
+    class Meta:
+        abstract = True
+
+class CommonMasterAbstract(models.Model):
+    name = models.CharField(max_length=50, null=False, unique=True)
+    is_active = models.BooleanField(default=True, verbose_name="Is Active", null=True)
+  
+    def __str__(self):
+        return f"{self.name}"
+    class Meta:
+        abstract = True 
+
+class StatusMaster(CommonMasterAbstract):
+    pass
+
+class ProductMaster(models.Model):
+    name=  models.CharField(max_length=150, blank=False,unique=True) 
+    is_active = models.BooleanField(null=True, default=True)
+   
+    class Meta:
+        abstract=True
+        constraints =  [models.UniqueConstraint(fields=['name', 'inventory_type'], name='Unique Product')]
+
+class Product(ProductMaster,BaseUserTrackedModel,BaseTimestampedModel):  #Product table
+     def __str__(self):
+        return f"{self.name}"       
+
+class InventoryMaster(models.Model):
+    name=models.CharField(max_length=150, blank=False)
+    connection_name=models.CharField(max_length=150, blank=False)
+    status=models.BooleanField(null=True, default=True)
+    class Meta:
+        abstract=True
+
+class Inventory(InventoryMaster,BaseUserTrackedModel,BaseTimestampedModel):
+     def __str__(self):
+        return f"{self.name}"
+
+
+class ChannelMaster(models.Model):
+    number=models.SmallIntegerField(null=False)
+    label=models.CharField(max_length=150, blank=False)
+    is_display=models.BooleanField(null=True, default=True)
+    display_order=models.SmallIntegerField(null=False)
+    unit_of_measure=models.CharField(max_length=150, blank=True,null=True)
+    class Meta:
+        abstract=True
+
+class Channel(ChannelMaster,BaseUserTrackedModel,BaseTimestampedModel):
+    def __str__(self):
+        return f"{self.label}"
+
+class Alert(models.Model):
+    name=  models.CharField(max_length=150, blank=False)
+    connection_name=models.ForeignKey(Inventory,on_delete=models.CASCADE,null=False)
+    
+
+
+  
+
